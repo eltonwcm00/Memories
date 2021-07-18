01103f03a095c824c628b0f1f3fe08e774b7cb56
eltonwcm00 <eltonwcm00@gmail.com>
2021-07-18 22:09:59 +0800

    backend setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
-// package.json -> "type": "module",
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const app = express();
 
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://elton:1234@memories.koalb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
+// eliminate the warning
+mongoose.set('useFindAndModify', false);
